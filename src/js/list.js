1121bd5002c6676af2c3f127a85cd59ac7f60830
jeongsaebom <saebom96@naver.com>
2019-12-07 16:37:07 +0900

    complete list frame

--- src/js/list.js
+++ src/js/list.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    
+    $("button.goto").click( function () {
+        window.location.href = $(this).parent().attr("data-link")
+    });
+
+    $("button.copy").click(function () {
+        $(this).parent().attr("data-link").select();
+        document.execCommand("copy");
+        alert("Copy completed.");
+    });
+
+    $("button.close").click(function (t) {
+        $(this).parent().parent().remove();
+    });
 });
