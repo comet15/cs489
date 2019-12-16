582ab67bd636f22b21afef6e6c8b8fa6bd0d1c6f
jeongsaebom <saebom96@naver.com>
2019-12-17 01:24:17 +0900

    link firebase

--- src/web/popup.js
+++ src/web/popup.js
@@ -1,9 +1,9 @@
-let urls = [];
-let myStorage = window.localStorage;
-let urlCount = 0;
-let bkg = chrome.extension.getBackgroundPage();
+var urls = [];
+var myStorage = window.localStorage;
+var urlCount = 0;
+var bkg = chrome.extension.getBackgroundPage();
 
-let button1 = document.getElementById('button1');
+var button1 = document.getElementById('button1');
 button1.onclick = function(element) {
 	chrome.tabs.query({'active':true, 'lastFocusedWindow':true}, function(tabs) {
 		var url = tabs[0].url;
@@ -13,8 +13,23 @@ button1.onclick = function(element) {
 	window.location.href="popup_ref.html";
 }
 
-let button2 = document.getElementById('button2');
+var button2 = document.getElementById('button2');
 button2.onclick = function(element) {
 	var newURL = 'file:///C:/Users/KunTae%20Park/Desktop/ce/cs489/src/web/list.html';
 	chrome.tabs.create({'url': newURL});
 }
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCggAvRjPps2mjPK2eqWjTpCoidwIZFSIg",
+	authDomain: "cs489-417f7.firebaseapp.com",
+	databaseURL: "https://cs489-417f7.firebaseio.com",
+	projectId: "cs489-417f7",
+	storageBucket: "cs489-417f7.appspot.com",
+	messagingSenderId: "388831884619",
+	appId: "1:388831884619:web:97fb2bba46306ac5e364ce",
+	measurementId: "G-3S9JJFSFY1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
--- src/web/popup_ref.js
+++ src/web/popup_ref.js
@@ -1,5 +1,31 @@
-let button_submit = document.getElementById("submit");
+var button_submit = document.getElementById("submit");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCggAvRjPps2mjPK2eqWjTpCoidwIZFSIg",
+	authDomain: "cs489-417f7.firebaseapp.com",
+	databaseURL: "https://cs489-417f7.firebaseio.com",
+	projectId: "cs489-417f7",
+	storageBucket: "cs489-417f7.appspot.com",
+	messagingSenderId: "388831884619",
+	appId: "1:388831884619:web:97fb2bba46306ac5e364ce",
+	measurementId: "G-3S9JJFSFY1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var database = firebase.database();
+var reference = database.ref("references");
+
+function writeData(name, url) {
+	database.ref('references/' + name).set({
+		link: url,
+		name: name
+	})
+}
 
 button_submit.onclick = function(element) {
+	writeData($("#description_input").val(), window.location.href);
 	window.close();
-}
+};
